831a3bd7fceca6e023cdcfc473146cc034c205f9
cc1005 <conorkampbell@hotmail.com>
2021-04-07 10:16:24 +0100

    Modified Dropper script to Cache references. Also created a Prefab of my falling obstacle.

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] float waitTime = 3f;
+    MeshRenderer objectRenderer;
+    Rigidbody objectRigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectRenderer = GetComponent<MeshRenderer>();
+        objectRigidbody = GetComponent<Rigidbody>();
+
+        objectRenderer.enabled = false;
+        objectRigidbody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Time.time > 3f)
+       if (Time.time > waitTime)
        {
-           GetComponent<Rigidbody>().useGravity = true;
+           objectRenderer.enabled = true;
+           objectRigidbody.useGravity = true;
        }
     }
 }
